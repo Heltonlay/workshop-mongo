0a846a6c3dbca79a44c0d3c2973eb1f8d07110ab
Heltonlay <heltonlay15@gmail.com>
2024-10-11 11:18:06 -0300

    Inserindo usuário por request body

--- src/main/java/com/danielpsilva/workshopmongo/services/UserService.java
+++ src/main/java/com/danielpsilva/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.danielpsilva.workshopmongo.domain.User;
+import com.danielpsilva.workshopmongo.dto.UserDTO;
 import com.danielpsilva.workshopmongo.exceptions.ObjectNotFoundException;
 import com.danielpsilva.workshopmongo.repositories.UserRepository;
 
@@ -25,5 +26,13 @@ public class UserService {
 		if (user.isEmpty())
 			throw new ObjectNotFoundException("Object not found");
 		return user.get();
+	}
+
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 }

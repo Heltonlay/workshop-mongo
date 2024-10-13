49b44391b595ffcbcf48807a006907fe5c4f7daa
heltonlay <heltonlay15@gmail.com>
2024-10-13 11:08:41 -0300

    Atualização de usuário com PUT

--- src/main/java/com/danielpsilva/workshopmongo/services/UserService.java
+++ src/main/java/com/danielpsilva/workshopmongo/services/UserService.java
@@ -37,6 +37,17 @@ public class UserService {
 		repo.deleteById(id);
 	}
 
+	public User update(User obj) {
+		User newObj = obj;
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
